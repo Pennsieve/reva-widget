810f898c70cf7a76b130ed453bdb36df2e4118ad
Eric Gauzens <ericgauzens@hotmail.com>
2024-12-29 11:55:03 -0500

    fixed error reporting

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -19,16 +19,14 @@
         />
       </el-select>
     </div>
-    <template v-if="error == null">
-      <div v-if="coordFilesLoading && subjectId != ''" class="vagus-viewer" v-loading="coordFilesLoading" element-loading-text="Loading 3D vagus tracing files..." />
-      <VagusTracingViewer v-else class="vagus-viewer" :coord-files=vagusCoordFiles />
-      <div v-if="microCtFilesLoading && subjectId != ''" class="file-selector" v-loading="microCtFilesLoading" element-loading-text="Loading subject files..." />
-      <FileSelector v-else class="file-selector" :files=vagusMicroCtFiles @file-selected="onFileSelected" />
-      <el-dialog v-if="selectedFile" class="dialog" v-model="isDialogOpen" @close="closeDialog" :title="selectedFile.name">
-        <VideoPlayer v-if="selectedFile.type === 'MP4'" :videoSrc="selectedFile.s3Url" />
-        <img v-if="selectedFile.type === 'PNG'" :src="selectedFile.s3Url" style="width: 100%; height: auto;" />
-      </el-dialog>
-    </template>
+    <div v-if="coordFilesLoading && subjectId != ''" class="vagus-viewer" v-loading="coordFilesLoading" element-loading-text="Loading 3D vagus tracing files..." />
+    <VagusTracingViewer v-else class="vagus-viewer" :coord-files=vagusCoordFiles />
+    <div v-if="microCtFilesLoading && subjectId != ''" class="file-selector" v-loading="microCtFilesLoading" element-loading-text="Loading subject files..." />
+    <FileSelector v-else class="file-selector" :files=vagusMicroCtFiles @file-selected="onFileSelected" />
+    <el-dialog v-if="selectedFile" class="dialog" v-model="isDialogOpen" @close="closeDialog" :title="selectedFile.name">
+      <VideoPlayer v-if="selectedFile.type === 'MP4'" :videoSrc="selectedFile.s3Url" />
+      <img v-if="selectedFile.type === 'PNG'" :src="selectedFile.s3Url" style="width: 100%; height: auto;" />
+    </el-dialog>
   </div>
 </template>
 
@@ -49,14 +47,6 @@ const coordFilesLoading = ref(true)
 const microCtFilesLoading = ref(true)
 const error = ref(null)
 const isDialogOpen = ref(false)
-
-try {
-  subjectIds.value = await fetchSubjectIds()
-} catch (err) {
-  error.value = "Failed to fetch subject ids: " + err.message;
-} finally {
-  subjectIdsLoading.value = false
-}
 
 watch(error, (newValue) => {
   if (newValue == null) { return }
@@ -90,29 +80,36 @@ watch(subjectId, async (newValue) => {
   try {
     vagusCoordFiles.value = await fetchCoordFiles(newValue)
   } catch (err) {
-    error.value = `Failed to fetch vagus coordinate files for subject with id ${newValue}: ` + err.message;
+    vagusCoordFiles.value = []
+    error.value = err.message;
   } finally {
     coordFilesLoading.value = false
   }
   try {
     vagusMicroCtFiles.value = await fetchMicroCtFiles(newValue)
   } catch (err) {
-    error.value = `Failed to fetch Micro CT files for subject with id ${newValue}: ` + err.message;
+    vagusMicroCtFiles.value = []
+    error.value = err.message;
   } finally {
     microCtFilesLoading.value = false
   }
 })
 
+try {
+  subjectIds.value = await fetchSubjectIds()
+} catch (err) {
+  error.value = err.message;
+} finally {
+  subjectIdsLoading.value = false
+}
+
 async function fetchSubjectIds() {
   try {
     const response = await fetch(`${import.meta.env.VITE_SPARC_API}/reva/subject-ids`)
-    if (!response.ok) {
-      throw new Error(`Error fetching subject ids! Status: ${response.status}`)
-    }
     const data = await response.json()
     return data['ids']
   } catch (error) {
-    throw new Error(`Error fetching subject ids!: ${error}`);
+    throw new Error(`Error fetching subject ids! ${error}`)
   }
 }
 
@@ -120,12 +117,12 @@ async function fetchCoordFiles(subjectId) {
   try {
     const response = await fetch(`${import.meta.env.VITE_SPARC_API}/reva/tracing-files/${subjectId}`)
     if (!response.ok) {
-      throw new Error(`Error fetching 3D tracing files! Status: ${response.status}`)
+      throw new Error(`Error fetching 3D tracing files for ${subjectId}! ${response.status}: ${response.statusText}`)
     }
     const data = await response.json()
     return data['files']
   } catch (error) {
-    throw new Error(`Error fetching 3D tracing files!: ${error}`);
+    throw new Error(error.message)
   }
 }
 
@@ -133,12 +130,15 @@ async function fetchMicroCtFiles(subjectId) {
   try {
     const response = await fetch(`${import.meta.env.VITE_SPARC_API}/reva/micro-ct-files/${subjectId}`)
     if (!response.ok) {
-      throw new Error(`Error fetching Micro CT files! Status: ${response.status}`)
+      if (response.status == '404')
+        throw new Error(`Failed to fetch files for ${subjectId} because they do not exist for this subject.`)
+      else
+        throw new Error(`Failed to fetch files for subject with id ${subjectId}! ${response.status}: ${response.statusText}`)
     }
     const data = await response.json()
     return data['files']
   } catch (error) {
-    throw new Error(`Error fetching Micro CT files!: ${error}`);
+    throw new Error(error.message)
   }
 }
 
